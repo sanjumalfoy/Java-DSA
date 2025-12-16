5d115c124965db22cb25c4ea7fd4715a3499ee2a
sanjeevan t <sanjeevan782000@gmail.com>
2025-12-16 23:25:54 +0530

    Updated few lines in celling and Added new Floor Program

--- sample/celling.java
+++ sample/celling.java
@@ -1,10 +1,17 @@
 public class celling {
     public static void main(String arg[]){
         int[] arr={2,4,6,33,56};
-        int target=30;
+        int target=100;
         System.out.println("Celling of this target is :"+findCelling(arr,target));
     }
+    
     public static int findCelling(int[] arr,int target){
+
+        // What if the target number is greater than the greatest number in the array
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
+
         int start=0;
         // int mid=0;
         int end=arr.length-1;
@@ -17,11 +24,12 @@ public class celling {
             if(arr[mid]<target){
                 start=mid+1;
             }
-            if(arr[mid]>target){
+            else{
                 end=mid-1;
             }
         }
 
+        // We return start as start id the smallest >=target , which describes the celling of a number
         return arr[start];
     }
 }
